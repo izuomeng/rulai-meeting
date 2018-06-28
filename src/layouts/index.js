081791ff92df5cb0d8d8f15db81b8402cc2d6f2a
izuomeng <dmate@foxmail.com>
2018-06-28 23:44:35 +0800

    Init

---
+++ src/layouts/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import withRouter from 'umi/withRouter'
+import router from 'umi/router'
+
+function Layout(props) {
+  // 拦截器，处理认证重定向等情况
+  const { location } = props
+  if (location.pathname === '/never') {
+    router.push('/')
+  }
+  return (
+    <React.Fragment>
+      <div>header</div>
+      {props.children}
+    </React.Fragment>
+  )
+}
+
+export default withRouter(Layout)
