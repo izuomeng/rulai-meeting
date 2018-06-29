24eb5185277c4e1ee7c9c465a7394c6d365963cb
izuomeng <dmate@foxmail.com>
2018-06-29 10:05:48 +0800

    Add antd

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { connect } from 'dva'
 import styles from './index.css'
+import { Button } from 'antd'
 
 function IndexPage() {
   return (
@@ -12,7 +13,9 @@ function IndexPage() {
           To get started, edit <code>src/index.js</code> and save to reload.
         </li>
         <li>
-          <a href="https://github.com/dvajs/dva">Getting Started</a>
+          <Button>
+            <a href="https://github.com/dvajs/dva">Getting Started</a>
+          </Button>
         </li>
       </ul>
     </div>
