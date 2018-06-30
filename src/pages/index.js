66279f2632116be6139ca35418f768514d5ed831
chenheri <332878447@qq.com>
2018-06-30 16:56:21 +0800

    index update

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,20 +2,33 @@ import React from 'react'
 import { connect } from 'dva'
 import styles from './index.css'
 import { Button } from 'antd'
+import styled from 'styled-components'
+import { InjectClass } from '@/utils/HOC'
+
+const StyledButton = styled(InjectClass(Button))`
+  font-size: 2em;
+  font-weight: 100;
+  height: 120px;
+  width: 300px;
+  margin-right: 120px;
+  background-color: rgb(4, 250, 168);
+  color: #001529;
+  :hover {
+    background: transparent;
+    color: #ffffff;
+    border: 5px solid #ffffff;
+  }
+`
 
 function IndexPage() {
   return (
     <div className={styles.normal}>
-      <h1 className={styles.title}>Yay! Welcome to dva!</h1>
-      <div className={styles.welcome} />
+      <div className={styles.tmp} />
+      <h1 className={styles.title}>Welcome to ru-lai meeting!</h1>
       <ul className={styles.list}>
         <li>
-          To get started, edit <code>src/index.js</code> and save to reload.
-        </li>
-        <li>
-          <Button type="primary">
-            <a href="https://github.com/dvajs/dva">Getting Started</a>
-          </Button>
+          <StyledButton>查看全部会议</StyledButton>
+          <StyledButton>发布会议</StyledButton>
         </li>
       </ul>
     </div>
