68fe7b2ee83fad8f2362889e962c4703c2f806f8
izuomeng <dmate@foxmail.com>
2018-06-30 22:16:48 +0800

    Add home page request

--- src/pages/home/components/Item.js
+++ src/pages/home/components/Item.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Card } from 'antd'
+import { Card, Icon } from 'antd'
 import Link from 'umi/link'
 import styled from 'styled-components'
 import { InjectClass } from '@/utils/HOC'
@@ -8,6 +8,7 @@ import { transTime } from '@/utils'
 const Tip = styled(
   InjectClass(props => (
     <span className={props.className}>
+      <Icon type={props.icon} style={{ marginRight: '8px' }} />
       {props.label}: {props.value}
     </span>
   ))
@@ -16,18 +17,32 @@ const Tip = styled(
   color: #aeb0b3;
 `
 
-const Item = ({ meeting }) => {
+const Item = ({ meeting, handleClick = () => {} }) => {
   return (
     <Card
+      hoverable
       title={meeting.title}
-      extra={<Link to={`/detail/${meeting.id}`}>收藏</Link>}
+      onClick={handleClick}
+      extra={
+        <Link to="/collection">
+          <Icon type="star" style={{ marginRight: '8px' }} />收藏
+        </Link>
+      }
       style={{ margin: '30px' }}
     >
       <div style={{ marginBottom: '20px' }}>{meeting.introduction}</div>
       <div>
-        <Tip label="机构" value={meeting.organization.name} />
-        <Tip label="开始日期" value={transTime(meeting.confBeginDate)} />
-        <Tip label="结束日前" value={transTime(meeting.ddlDate)} />
+        <Tip icon="solution" label="机构" value={meeting.organization.name} />
+        <Tip
+          icon="clock-circle-o"
+          label="开始日期"
+          value={transTime(meeting.confBeginDate)}
+        />
+        <Tip
+          icon="clock-circle"
+          label="结束日前"
+          value={transTime(meeting.ddlDate)}
+        />
       </div>
     </Card>
   )
