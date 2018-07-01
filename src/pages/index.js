786f898292f463e0cc305f08acbeb5d4a1bc94eb
chenheri <332878447@qq.com>
2018-07-01 11:53:12 +0800

    Update index style

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,19 @@
 import React from 'react'
 import { connect } from 'dva'
-import { Button } from 'antd'
+//import { Button } from 'antd'
 import styled from 'styled-components'
-import { InjectClass } from '@/utils/HOC'
+//import { InjectClass } from '@/utils/HOC'
 import styles from './index.css'
 
-const StyledButton = styled(InjectClass(Button))`
+const StyledButton = styled.button`
   font-size: 1.5em;
   font-weight: 500;
   height: 80px;
+  outline: none;
   width: 200px;
   border-radius: 14px;
-  margin-right: 120px;
+  margin-left: 60px;
+  margin-right: 60px;
   background: transparent;
   border: 4px solid #ffffff;
   color: #ffffff;
@@ -25,14 +27,13 @@ const StyledButton = styled(InjectClass(Button))`
 function IndexPage() {
   return (
     <div className={styles.normal}>
-      <div className={styles.tmp} />
-      <h1 className={styles.title}>Welcome to ru-lai meeting!</h1>
-      <ul className={styles.list}>
-        <li>
-          <StyledButton>查看会议</StyledButton>
-          <StyledButton>发布会议</StyledButton>
-        </li>
-      </ul>
+      <div>
+        <div className={styles.title}>Welcome to Lein Meeting!</div>
+      </div>
+      <div className={styles.list}>
+        <StyledButton>查看会议</StyledButton>
+        <StyledButton>发布会议</StyledButton>
+      </div>
     </div>
   )
 }
