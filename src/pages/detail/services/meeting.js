7996be5fb94038a66a0f91db9cdab40834ec18d9
zuomeng <weekhimup@gmail.com>
2018-07-02 14:32:42 +0800

    Update meeting.js

--- src/pages/detail/services/meeting.js
+++ src/pages/detail/services/meeting.js
@@ -1,7 +1,7 @@
 import request from '@/utils/request'
 
 export function getMeetingsByID(id) {
-  return request(`/api/user/conference/1`)
+  return request(`/tapi/user/conference/1`)
 }
 
 // export function getPaperSample(){
